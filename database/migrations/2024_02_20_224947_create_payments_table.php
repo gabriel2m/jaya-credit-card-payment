d3e0a5be78cebf5c179b11d9dff8d1dd2f897d9f
gabriel2m <gabriel2m.contact@gmail.com>
2024-02-24 15:54:25 -0300

    fix: update transaction_amount total digits

--- database/migrations/2024_02_20_224947_create_payments_table.php
+++ database/migrations/2024_02_20_224947_create_payments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->float('transaction_amount');
+            $table->float('transaction_amount', 11);
             $table->integer('installments');
             $table->string('token');
             $table->string('payment_method_id');
